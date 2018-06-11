d188ddb0908051f263d41d6b438551a9c4c46c5d
Angelo Guerrero Medina <angelomedinabarraza@gmail.com>
2018-06-10 22:40:13 -0600

    Replicacion de tablas: funciona

--- conn.php
+++ conn.php
@@ -1,38 +1,43 @@
 <?php
 
-if ($_GET['func']=='conectar()')
+if ($_GET['func']=='conectarOrigen()')
 {
-    conectar($_GET['usuario'],$_GET['contraseña'],$_GET['ip'],$_GET['puerto']);
+    conectarOrigen($_GET['usuario'],$_GET['contraseña'],$_GET['ip'],$_GET['puerto']);
 }
 
-if ($_GET['func']=='newConection()')
+if ($_GET['func']=='conexionDBorigen()')
 {
-    newConection($_GET['usuario'],$_GET['contraseña'],$_GET['ip'],$_GET['puerto'],$_GET['bd']);
+    conexionDBorigen($_GET['usuario'],$_GET['contraseña'],$_GET['ip'],$_GET['puerto'],$_GET['bd']);
 }
 
-function conectar($usuario,$contraseña,$ip,$puerto){
+if ($_GET['func']=='conexionDBdestino()')
+{
+    conexionDBdestino($_GET['usuario'],$_GET['contraseña'],$_GET['ip'],$_GET['puerto'],$_GET['bd']);
+}
+
+if ($_GET['func']=='columnasTablas()')
+{
+    columnasTablas($_GET['usuario'],$_GET['contraseña'],$_GET['ip'],$_GET['puerto'],$_GET['bd'],$_GET['tabla']);
+}
+
+if ($_GET['func']=='crearTablas()')
+{
+    crearTablas($_GET['usuario'],$_GET['contraseña'],$_GET['ip'],$_GET['puerto'],$_GET['bd'],$_GET['tabla'],$_GET['columnas']);
+}
+
+
+function conectarOrigen($usuario, $contraseña, $ip, $puerto){
     $dbname          = "postgres";
     $conexion        =  @pg_connect("host=$ip port=$puerto dbname=$dbname user=$usuario password=$contraseña");
 
     if( !$conexion) {
         echo "error";
     }else{
-        seleccionDB($conexion);
+        seleccionDBorigen($conexion);
     }
 }
 
-function newConection($usuario,$contraseña,$ip,$puerto,$bd){
-    $dbname          =  $bd;
-    $conexion        =  @pg_connect("host=$ip port=$puerto dbname=$dbname user=$usuario password=$contraseña");
-
-    if( !$conexion) {
-        echo "error";
-    }else{
-        echo "exito";
-    }
-}
-
-function seleccionDB($conexion){
+function seleccionDBorigen($conexion){
 
     $sql = "SELECT datname FROM pg_database WHERE datistemplate = false;";
     $contests = pg_query( $conexion, $sql );
@@ -48,5 +53,104 @@ function seleccionDB($conexion){
     }
 }
 
+function conexionDBorigen($usuario, $contraseña, $ip, $puerto, $bd){
+    $dbname          =  $bd;
+    $conexion        =  @pg_connect("host=$ip port=$puerto dbname=$dbname user=$usuario password=$contraseña");
 
+    if( !$conexion) {
+        echo "error";
+    }else{
+        $sql = "SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';";
+        $contests = pg_query( $conexion, $sql );
 
+        if($contests ){
+
+            $resultArray = pg_fetch_all($contests);
+            echo json_encode($resultArray);
+            pg_close($conexion);
+
+        }else{
+            echo "error";
+        }
+    }
+}
+
+function conexionDBdestino($usuario, $contraseña, $ip, $puerto, $bd){
+    $dbname          =  "postgres";
+    $conexion        =  @pg_connect("host=$ip port=$puerto dbname=$dbname user=$usuario password=$contraseña");
+
+    if( !$conexion) {
+        echo "error";
+    }else{
+        $sql = "CREATE DATABASE $bd;";
+        $contests = pg_query( $conexion, $sql );
+
+        if($contests ){
+
+            dblink($usuario, $contraseña, $ip, $puerto, $bd);
+
+        }else{
+            echo "error";
+        }
+    }
+}
+
+function dblink($usuario, $contraseña, $ip, $puerto, $bd){
+
+    $dbname          =  $bd;
+    $conexion        =  @pg_connect("host=$ip port=$puerto dbname=$dbname user=$usuario password=$contraseña");
+
+    $sql = "create extension dblink;";
+    $contests = pg_query( $conexion, $sql );
+
+    if($contests ){
+        echo "exito";
+
+    }else{
+        echo "error";
+    }
+};
+
+function columnasTablas($usuario, $contraseña, $ip, $puerto, $bd,$tabla){
+    $dbname          =  $bd;
+    $conexion        =  @pg_connect("host=$ip port=$puerto dbname=$dbname user=$usuario password=$contraseña");
+
+    if( !$conexion) {
+        echo "error";
+    }else{
+        $sql = "SELECT column_name,data_type from information_schema.columns where table_name= '$tabla'";
+        $contests = pg_query( $conexion, $sql );
+
+        if($contests ){
+
+            $resultArray = pg_fetch_all($contests);
+            echo json_encode($resultArray);
+            pg_close($conexion);
+
+        }else{
+            echo "error";
+        }
+    }
+}
+
+function crearTablas($usuario, $contraseña, $ip, $puerto, $bd,$tabla,$columnas){
+
+    $dbname          =  $bd;
+    $conexion        =  @pg_connect("host=$ip port=$puerto dbname=$dbname user=$usuario password=$contraseña");
+
+    if( !$conexion) {
+        echo "error";
+    }else{
+        $sql = "CREATE TABLE $tabla ($columnas);";
+        $contests = pg_query( $conexion, $sql );
+
+        if($contests ){
+
+            echo "exito";
+            pg_close($conexion);
+
+        }else{
+            echo "error";
+        }
+    }
+}
--- method.js
+++ method.js
@@ -1,9 +1,28 @@
+var tablas = [];
+
+//conexiones de origen
 var Gusuario     ="";
 var Gcontraseña  ="";
 var GIP          ="";
 var Gpuerto      ="";
+var Gdb          ="";
 
-function setDatos() {
+//conexiones de destino
+var Dusuario     ="";
+var Dcontraseña  ="";
+var DIP          ="";
+var Dpuerto      ="";
+var Ddb          ="";
+
+
+//configuracion
+var tipo          ="";
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Descripcion: metodos origen
+
+function inputsIniciales() {
     $("#usuario").val("postgres");
     $("#constrasena").val("deathnote");
     $("#direccionip").val("localhost");
@@ -15,7 +34,7 @@ function setDatos() {
     $("#puertoServidorD").val("5432");
 }
 
-function conexion() {
+function conexionOrigen() {
 
     var usuario    = document.getElementById('usuario').value;
     var contraseña = document.getElementById('constrasena').value;
@@ -51,14 +70,14 @@ function conexion() {
 
                                 var value = obj[key];
 
-                                addOptions("select-bd-postgres", value.toString());
+                                agregarOpcionSelect("select-bd-postgres", value.toString());
                             }
                         }
                     });
 
                 }else{
                     swal({
-                        title: "Error de conexion!",
+                        title: "Error de conexionOrigen!",
                         text:  "Verifique los datos ingresados!",
                         icon: "warning",
                         button: "OK!",
@@ -68,12 +87,13 @@ function conexion() {
             else{console.log(this.statusText, this.status)}
         }
     };
-    xhttp.open("GET", "conn.php?func=conectar()&usuario="+usuario.toString()+"&contraseña="+contraseña.toString() +"&ip="+IP.toString() +"&puerto="+puerto.toString(), true);
+    xhttp.open("GET", "conn.php?func=conectarOrigen()&usuario="+usuario.toString()+"&contraseña="+contraseña.toString() +"&ip="+IP.toString() +"&puerto="+puerto.toString(), true);
     xhttp.send();
 }
 
-function setDB(){
+function conexionDBorigen(){
 
+    tablas          = [];
     var bd          = document.getElementById('select-bd-postgres').value;
     var replicacion = document.getElementById('select-replicar').value;
 
@@ -84,6 +104,7 @@ function setDB(){
             if(this.statusText== "OK" && this.status == 200) {
 
                 if(this.response.toString() != "error") {
+
                     swal({
                         title: "Conexion exitosa!",
                         text:  bd,
@@ -93,12 +114,26 @@ function setDB(){
 
                         $("#baseD").val(bd.toString());
 
-                        replicar(replicacion.toString(),bd.toString());
+                        var json = this.response;
+                        var arr = JSON.parse(json);
+
+                        for (var i = 0; i < arr.length; i++){
+
+                            var obj = arr[i];
+                            for (var key in obj){
+
+                                var value = obj[key];
+
+                                tablas.push(value.toString());
+                            }
+                        }
+                        Gdb  = bd.toString();
+                        tipo = replicacion.toString();
                     });
 
                 }else{
                     swal({
-                        title: "Error de conexion!",
+                        title: "Error de conexionOrigen!",
                         text:  "Verifique los datos ingresados!",
                         icon: "warning",
                         button: "OK!",
@@ -108,14 +143,14 @@ function setDB(){
             else{console.log(this.statusText, this.status)}
         }
     };
-    xhttp.open("GET", "conn.php?func=newConection()&usuario="+Gusuario+"&contraseña="+Gcontraseña+"&ip="+GIP+"&puerto="+Gpuerto+"&bd="+bd, true);
+    xhttp.open("GET", "conn.php?func=conexionDBorigen()&usuario="+Gusuario+"&contraseña="+Gcontraseña+"&ip="+GIP+"&puerto="+Gpuerto+"&bd="+bd, true);
     xhttp.send();
 
 
 
 }
 
-function addOptions(domElement, array) {
+function agregarOpcionSelect(domElement, array) {
 
     var select = document.getElementsByName(domElement)[0];
     var option = document.createElement("option");
@@ -124,7 +159,138 @@ function addOptions(domElement, array) {
 
 }
 
-function replicar(tipo,db) {
-    console.log("Replicacion: "+tipo+"db "+db);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function conexionDestino() {
+
+    var usuario    = document.getElementById('usuarioD').value;
+    var contraseña = document.getElementById('constrasenaD').value;
+    var IP         = document.getElementById("direccionipD").value;
+    var puerto     = document.getElementById("puertoServidorD").value;
+    var db         = document.getElementById("baseD").value;
+
+
+    Dusuario     = usuario.toString();
+    Dcontraseña  = contraseña.toString();
+    DIP          = IP.toString();
+    Dpuerto      = puerto.toString();
+    Ddb          = db.toString();
+
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            if(this.statusText== "OK" && this.status == 200) {
+
+                if(this.response.toString() != "error") {
+                    swal({
+                        title: "Conexion exitosa!",
+                        text:  "Se creo: "+db+"!",
+                        icon:  "success",
+                        button: "OK",
+                    }).then((value) => {
+
+                        agregarOpcionSelect("select-bd-postgres", db.toString());
+
+                        columnasTablas();
+
+                    });
+                }else{
+                    swal({
+                        title:  "Error de conexion destino!",
+                        text:   "Verifique los datos ingresados!",
+                        icon:   "warning",
+                        button: "OK!",
+                    });
+                }
+            }
+            else{console.log(this.statusText, this.status)}
+        }
+    };
+    xhttp.open("GET", "conn.php?func=conexionDBdestino()&usuario="+Dusuario+"&contraseña="+Dcontraseña+"&ip="+DIP+"&puerto="+Dpuerto+"&bd="+Ddb, true);
+    xhttp.send();
+
 
 }
+
+function columnasTablas(){
+
+    swal({
+        title:  "Replicacion de tablas!",
+        text:   "Se replicaran "+tablas.length+" tablas",
+        icon:   "success",
+        button: "OK",
+
+    }).then((value) => {
+
+        tablas.forEach(function (tabla) {
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+
+                if (this.readyState == 4 && this.status == 200) {
+                    if(this.statusText== "OK" && this.status == 200) {
+
+                        var columnas = "";
+
+                        var json = this.response;
+                        var arr = JSON.parse(json);
+
+                        for (var i = 0; i < arr.length; i++){
+
+                            var obj = arr[i];
+
+                            for (var key in obj){
+
+                                var value = obj[key];
+                                columnas = columnas + " " + value;
+                            }
+                            columnas = columnas + ",";
+                        }
+                        columnas = columnas.substring(0,columnas.length-1);
+
+                        crearTabla(tabla, columnas);
+                    }
+                    else{console.log(this.statusText, this.status)}
+                }
+            };
+            xhttp.open("GET", "conn.php?func=columnasTablas()&usuario="+Gusuario+"&contraseña="+Gcontraseña+"&ip="+GIP+"&puerto="+Gpuerto+"&bd="+Gdb+"&tabla="+tabla.toString(), true);
+            xhttp.send();
+
+        });
+
+    });
+}
+
+function crearTabla(tabla, columnas) {
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            if(this.statusText== "OK" && this.status == 200) {
+
+                if(this.response.toString() != "error") {
+
+                    swal({
+                        title: "Cracion de tablas!",
+                        text:  "Tabla: "+tabla,
+                    });
+
+                }else{
+                    swal({
+                        title: "Error de replicacion de tablas!",
+                        text:  "Verifique los datos ingresados!",
+                        icon: "warning",
+                        button: "OK!",
+                    });
+                }
+            }
+            else{console.log(this.statusText, this.status)}
+        }
+    };
+    xhttp.open("GET", "conn.php?func=crearTablas()&usuario="+Dusuario+"&contraseña="+Dcontraseña+"&ip="+GIP+"&puerto="+Dpuerto+"&bd="+Ddb+"&tabla="+tabla+"&columnas="+columnas, true);
+    xhttp.send();
+}
+
